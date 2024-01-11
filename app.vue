311198649e74c20800b1c9b9f6de9b48c3d62136
m-hamanaka <hamanaka@creativebank.co.jp>
2024-01-11 19:04:05 +0900

    オブジェクトをまとめてリアクティブにする

--- app.vue
+++ app.vue
@@ -2,29 +2,31 @@
   const widthInit = Math.round(Math.random() * 10);
   const heightInit = Math.round(Math.random() * 10);
 
-  //Nuxtは、ref computedは オートインポート
+  //縦横の長さをまとめたひとつのオブジェクトリレラルを用意
+  //オブジェクトをまとめてリアクティブなテンプレート変数にする
+  const rectangle = reactive({
+    width: widthInit,
+    height: heightInit
+  });
 
-  //リアクティブなテンプレート変数
-  const width = ref(widthInit);
-  const height = ref(heightInit);
   //計算結果をリアクティブ
   const area = computed(
     (): number => {
-      return width.value * height.value;
+      return rectangle.width * rectangle.height;
     }
   );
 
-//.valueプロパティへ代入、変数の値変更
+//オブジェクト内のプロパティという形でアクセス
 setInterval(
   ():void => {
-    width.value = Math.round(Math.random() * 10);
-    height.value = Math.round(Math.random() * 10);
+    rectangle.width = Math.round(Math.random() * 10);
+    rectangle.height = Math.round(Math.random() * 10);
   },
   1000
 );
 </script>
 
 <template>
-  <p>縦が{{height}}で横が{{width}}の長方形の面積は{{area}}</p>
+  <p>縦が{{rectangle.height}}で横が{{rectangle.width}}の長方形の面積は{{area}}</p>
 
 </template>
