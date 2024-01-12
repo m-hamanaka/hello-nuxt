495e54b0b05d8402a72e914eb1fe37f7df66162e
m-hamanaka <hamanaka@creativebank.co.jp>
2024-01-12 18:52:00 +0900

    ボタンクリックに応じたメッセージ更新

--- app.vue
+++ app.vue
@@ -1,32 +1,37 @@
+
+<!-- ボタンをクリックすると、msg の値が "Hello" とボタンのテキストである
+"こんにちは" と組み合わされて更新され、それが表示されるという動作 -->
+
 <script setup lang="ts">
-  const widthInit = Math.round(Math.random() * 10);
-  const heightInit = Math.round(Math.random() * 10);
 
-  //縦横の長さをまとめたひとつのオブジェクトリレラルを用意
-  //オブジェクトをまとめてリアクティブなテンプレート変数にする
-  const rectangle = reactive({
-    width: widthInit,
-    height: heightInit
-  });
+  //ref 関数を使用して、msg というリアクティブな変数を作成し、初期値を "まだ" に設定
+  const msg = ref("まだ");
 
-  //計算結果をリアクティブ
-  const area = computed(
-    (): number => {
-      return rectangle.width * rectangle.height;
-    }
-  );
+  //ボタンがクリックされたときに呼び出される関数 onButtonClick を定義
+  //メソッドを定義 アロー関数を変数に代入 constメソッド名 = (引数): void => {
+  //label と event という2つの引数を受け取り、
+  //ボタンのテキストとして label を、
+  //ボタン要素のテキストコンテンツとして event.target から取得したものを組み合わせて、
+  //msg の値を更新
+  const onButtonClick = (label: string, event: Event): void => {
 
-//オブジェクト内のプロパティという形でアクセス
-setInterval(
-  ():void => {
-    rectangle.width = Math.round(Math.random() * 10);
-    rectangle.height = Math.round(Math.random() * 10);
-  },
-  1000
-);
+    //TypeScript における型キャストの書き方
+    //イベントオブジェクト (event) から target プロパティを取得し、
+    //その値を HTMLButtonElement 型にキャスト
+    const target = event.target as HTMLButtonElement;
+    const text = target.innerText;
+    msg.value = `${label}と${text}`;
+  };
 </script>
 
 <template>
-  <p>縦が{{rectangle.height}}で横が{{rectangle.width}}の長方形の面積は{{area}}</p>
+  <!-- ボタンがクリックされると更新 -->
+  <p>{{ msg }}</p>
 
+  <!-- メソッドをv-onディレクティブでイベントハンドラとする -->
+  <!-- v-on:click ディレクティブは、クリックイベントが発生したときに指定されたメソッドを呼び出す -->
+  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
+
+  <!-- $event は、クリックイベント自体を引数として渡すための特殊な構文
+  "Hello" という文字列を label として onButtonClick メソッドに渡す -->
 </template>
